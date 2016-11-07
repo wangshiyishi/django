7f2b9ee31ec60e72312c7f5df26ad6bd9b1767ff
wangshiyishi <13696139713@163.com>
2016-11-07 17:15:25 +0800

    1

--- learn/learn/admin.py
+++ learn/learn/admin.py
@@ -1,6 +1,15 @@
 from  django.contrib import admin
 from app01 import models
 
-admin.site.register(models.BBS)
+
+class BBS_admin(admin.ModelAdmin):
+    list_display = ('title', 'summary', 'author',' signature', 'view_count', 'cerated_at' )
+    list_filter = ('created_at',)
+    search_fields = ('title','author__user__username')
+    def signature(self,obj):
+        return obj.author. signature
+     signature.shortDescription = 'HAA'
+
+admin.site.register(models.BBS,BBS_admin)
 admin.site.register(models.Category)
 admin.site.register(models.BBs_user)
