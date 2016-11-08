0b0a4dfe20d393425c478d705f0894b0bd7a6b68
wangshiyishi <13696139713@163.com>
2016-11-08 14:23:17 +0800

    1

--- learn/JC2.PY.py
+++ learn/JC2.PY.py
@@ -1,3 +1,13 @@
+django-admin stratproject mysite
+python manage.py starapp learn
+
+# coding:utf-8
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse(u"欢迎光临 自强学堂!")
+
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
@@ -8,3 +18,4 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
--- learn/learn/settings.py
+++ learn/learn/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'study'
 )
 
 MIDDLEWARE_CLASSES = (
